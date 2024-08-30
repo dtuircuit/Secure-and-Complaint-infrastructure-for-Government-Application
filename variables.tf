ffff72d3a247b3c025236d4e6426f521027a1fcb
Dadrion <dtuircuit@gmail.com>
2024-08-30 15:55:23 -0500

    moved large files to .gitignore

--- variables.tf
+++ variables.tf
@@ -1,115 +1,113 @@
-#AWS region
-variable "aws_region" {
-    description = "The AWS region to deploy resources in"
-    default = "us-east-2"
+# VPC Configuration
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-#AWS Access ID and Secret Access ID Key
-variable "aws_access_key_id" {
-    description = "AWS Access Key ID"
-    type = string
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-2"
 }
 
-variable "aws_secret_access_key" {
-    description = "AWS Secret Access Key"
-    type = string
-}
-
-#VPC ID (for referencing existing VPCs)
-variable "vpc_id"{
-    description ="The ID of he VPC to asscociate with subnets and other resources"
-    type = string
-}
-
-#VPC CIDR Block
-variable "vpc_cidr_block" {
-    description = "The CIDR block for the VPC"
-    default = "10.0.0.0/16"
-}
-
-#Public Subnet CIDR Blocks
+# Subnet Configuration
 variable "public_subnet_cidrs" {
-    description = "The CIDR blocks for the public subnets"
-    type = list(string)
-    default = [
-        "10.0.0.0/24",
-        "10.0.1.0/24",
-        "10.0.2.0/24"
-    ]
-}
-# Private Subnet CIDR Blocks (Apps Instances)
-variable "private_app_subnet_cidrs" {
-    description = "The CIDR blocks for the private subnet for EC2 instances"
-    type = list(string)
-    default = [
-        "10.0.3.0/24",
-        "10.0.4.0/24",
-        "10.0.5.0/24"
-    ]
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
-#Private Subnet CIDR Blocks (Database)
+variable "private_ec2_subnet_cidrs" {
+  description = "CIDR blocks for private subnets (EC2 instances)"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
 variable "private_db_subnet_cidrs" {
-    description = "The CIDR blocks for the private subnets for databases"
-    type = list(string)
-    default = [
-        "10.0.6.0/24",
-        "10.0.7.0/24",
-        "10.0.8.0/24"
-    ]
+  description = "CIDR blocks for private subnets (RDS instances)"
+  type        = list(string)
+  default     = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
 }
-#EC2 Instance type
+
+# EC2 Configuration
 variable "ec2_instance_type" {
-    description = "The type of EC2 instances to launch"
-    default ="t2.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
-#AMI ID for ECC2 Instances
 variable "ami_id" {
-    description = "THE AMI ID for the EC2 instances"
-    default = "ami-0490fddec0cbeb88b"
+  description = "AMI ID for the EC2 instances"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
 }
 
-#IAM Role names
-variable "ec2_role_name" {
-    description = "The name of the IAM role for the EC2 instances"
-    default = "ec2_role"
+# RDS Configuration
+variable "db_instance_class" {
+  description = "RDS instance type"
+  type        = string
+  default     = "db.t2.micro"
 }
 
-variable "api_gateway_role_name" {
-    description = "The name of the IAM role for the API Gateway"
-    default = "api_gateway_role"
+variable "db_allocated_storage" {
+  description = "RDS allocated storage"
+  type        = number
+  default     = 20
 }
 
-#Security Group
-variable "ec2_secuirty_group_name" {
-    description = "The name of te security group forthe EC2 instances"
-    default = "EC2-SG"
+variable "db_name" {
+  description = "Name of the database"
+  type        = string
+  default     = "mydb"
 }
 
-variable "rds_security_group_name" {
-    description = "The name of the security group fo the RDS database"
-    default = "RDS-SG"
+variable "db_username" {
+  description = "Database username"
+  type        = string
+  default     = "admin"
 }
 
-#IAM Policies
-variable "ec2_policy_name" {
-description = "The name of the IAM policy for the EC2 instances"
-default = "ec2_policy"
-
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  default     = "password"
 }
 
-variable "api_gateway_policy_name" {
-    description = "The name of the IAM policy for the API Gateway"
-    default = "api_gateway_policy"
+variable "db_backup_retention_period" {
+  description = "Number of days to retain backups"
+  type        = number
+  default     = 7
 }
 
-#Tags 
-variable "tags" {
-    description = "Tags to apply to all resources"
-    type = map(string)
-    default = {
-        Enviroment = "Dev"
-        Project = "GovTech"
-    }
+# API Gateway and ALB Configuration
+variable "api_gateway_name" {
+  description = "Name of the API Gateway"
+  type        = string
+  default     = "govtech-api"
+}
+
+variable "alb_name" {
+  description = "Name of the Application Load Balancer"
+  type        = string
+  default     = "govtech-alb"
+}
+
+variable "alb_internal" {
+  description = "Whether the ALB is internal or external"
+  type        = bool
+  default     = false
+}
+
+# IAM Role Configuration
+variable "iam_role_name" {
+  description = "Name of the IAM role"
+  type        = string
+  default     = "govtech-ec2-role"
+}
+
+variable "iam_policy_name" {
+  description = "Name of the IAM policy"
+  type        = string
+  default     = "govtech-ec2-policy"
 }
